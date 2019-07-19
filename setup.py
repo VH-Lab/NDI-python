0698f2ea61547e681248cadd19af0f6ff14af6f8
Nell Hardcastle <nell@squishymedia.com>
2019-07-19 14:12:11 -0700

    Add basic setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='ndi',
+      version='1.0',
+      description='Neuroscience Data Interface',
+      author='Squishymedia',
+      author_email='info@squishymedia.com',
+      packages=['ndi', 'ndi.schema'])
