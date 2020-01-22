60a996fa0bf8235c4bdae5ce1f552042cf78e69c
David Nishikawa <david@squishymedia.com>
2020-01-21 16:10:06 -0800

    update abstract class methods

--- ndi/database/base_db.py
+++ ndi/database/base_db.py
@@ -21,17 +21,50 @@ class BaseDB(ABC):
         pass
 
     @abstractmethod
-    def add_experiment(self):
+    def add_experiment(self, experiment):
         pass
 
     @abstractmethod
-    def create_collections(self):
+    def __create_collections(self):
         pass
 
     @abstractmethod
-    def add(self):
+    def create_collection(self, name, **fields)
+        pass
+
+
+    @abstractmethod
+    def add(self, ndi_object):
         pass
 
     @abstractmethod
-    def find(self):
+    def find(self, ndi_class, **query):
         pass
+    
+    @abstractmethod
+    def find_by_id(self, ndi_class, id_):
+        pass
+
+    @abstractmethod
+    def update(self):
+        # params tbd
+        pass
+    
+    @abstractmethod
+    def update_by_id(self, ndi_class, id, payload):
+        pass
+
+    @abstractmethod
+    def upsert(self, ndi_objects):
+        pass
+
+    @abstractmethod
+    def delete(self, ndi_entity, **query):
+        pass
+    
+    @abstractmethod
+    def delete_by_id(self, ndi_class, id_):
+        pass
+
+
+
