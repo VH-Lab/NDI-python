1b45096539b4f26f1d78f35a754ffde9a9f259a0
David Nishikawa <david@squishymedia.com>
2020-01-23 12:36:19 -0800

    pull daq_reader ot of daq_system

--- ndi/daqsystem/probe.py
+++
@@ -1,9 +0,0 @@
-from ndi.schema.Probe import Probe as ProbeInterface
-
-class Probe(ProbeInterface):
-    """
-    An instrument that makes a measurement or provides stimulation. Examples include an electrode, a camera, a 2-photon microscope, a visual stimulus monitor, a nose-poke, a feeder.
-    """
-    def __init__(self, name, reference):
-        self.name = name
-        self.reference = reference
