6bd64e315310e7bc726a63cf0bf8cadc0038d5c0
Tommy Tran <tommy@squishymedia.com>
2020-05-06 15:01:10 -0700

    made tests for binary collection

---
+++ tests/database/filesystem/test_binary_collection.py
@@ -0,0 +1,51 @@
+from ndi.database.file_system import BinaryCollection
+from pathlib import Path
+import numpy as np
+
+randarray = np.random.random(200000)
+
+class TestBinaryCollection:
+    def setup(self):
+        self.collection = BinaryCollection('./test_db', 'document')
+        self.collection_dir = self.collection.collection_dir
+
+    def test_directory_creation(self):
+        self.setup()
+        assert self.collection_dir.exists()
+        assert self.collection_dir.is_dir()
+
+        self.tear_down()
+        assert not self.collection_dir.exists()
+
+    def test_write(self):
+        self.setup()
+        self.collection.write('fake_id', randarray)
+
+        assert (self.collection_dir / 'fake_id.bin').exists()
+        assert (self.collection_dir / 'fake_id.bin').is_file()
+
+    def test_read(self):
+        result = self.collection.read('fake_id')
+        assert type(result) == np.ndarray
+        for i, item in enumerate(result):
+            assert item == randarray[i]
+        
+        result = self.collection.read('fake_id', 300, 40000)
+        assert type(result) == np.ndarray
+        for i, item in enumerate(result):
+            assert item == randarray[300:40000][i]
+    
+        self.tear_down()
+        assert not self.collection_dir.exists()
+
+    def tear_down(self):
+        rmrf(self.collection_dir)
+
+
+def rmrf(directory):
+    for item in directory.iterdir():
+        if item.is_dir():
+            rmrf(item)
+        elif item.is_file():
+            item.unlink()
+    directory.rmdir()
