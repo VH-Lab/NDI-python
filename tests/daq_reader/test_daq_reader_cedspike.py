f52f82b3b205acde3250b409e7dac785a12ec13b
Tommy Tran <tommy@squishymedia.com>
2020-05-11 15:49:22 -0700

    made a variable out of 21

--- tests/daq_reader/test_daq_reader_cedspike.py
+++ tests/daq_reader/test_daq_reader_cedspike.py
@@ -1,6 +1,8 @@
 from ndi.daqreaders.cedspike2 import CEDSpike2
 
+
 class TestCEDSpike2:
     def test_samplerate(self):
         daq_reader = CEDSpike2('./tests/data/daqreaders/cedspike2/example1.smr')
-        assert daq_reader.samplerate(21) == 11111.1111
+        channel_number = 21
+        assert daq_reader.samplerate(channel_number) == 11111.1111
