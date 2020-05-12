b11ac757fb3be3be6091efb65f81f7c39187ba25
Tommy Tran <tommy@squishymedia.com>
2020-05-12 16:35:00 -0700

    Started DaqReader abstract class, implemented samplerate method + tests

--- tests/daq_reader/test_daq_reader_cedspike.py
+++ tests/daq_reader/test_daq_reader_cedspike.py
@@ -5,4 +5,15 @@ class TestCEDSpike2:
     def test_samplerate(self):
         daq_reader = CEDSpike2('./tests/data/daqreaders/cedspike2/example1.smr')
         channel_number = 21
-        assert daq_reader.samplerate(channel_number) == 11111.1111
+        expected_samplerate = 1/.00009
+        assert daq_reader.samplerate(channel_number) == expected_samplerate
+
+        daq_reader = CEDSpike2('./tests/data/daqreaders/cedspike2/example2.smr')
+        channel_number = 21
+        expected_samplerate = 1/.00009
+        assert daq_reader.samplerate(channel_number) == expected_samplerate
+
+        daq_reader = CEDSpike2('./tests/data/daqreaders/cedspike2/example3.smr')
+        channel_number = 21
+        expected_samplerate = 1/.00009
+        assert daq_reader.samplerate(channel_number) == expected_samplerate
