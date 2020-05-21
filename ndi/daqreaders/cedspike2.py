eee9ae08c79f4548bcfbcb22a022338c249fbdd1
Tommy Tran <tommy@squishymedia.com>
2020-05-21 00:59:03 -0700

    implemented readevents + parametrized tests

--- ndi/daqreaders/cedspike2.py
+++ ndi/daqreaders/cedspike2.py
@@ -13,9 +13,11 @@ class CEDSpike2(DaqReader):
 
     def readchannel(self, channel_number, start_index=0, end_index=None):
         channel_id = channel_number - 1
-        raw_signal = self.__reader.get_analogsignal_chunk(
-            channel_ids=[channel_id])
-        return self.__reader.rescale_signal_raw_to_float(raw_signal, channel_ids=[channel_id])[start_index:end_index, 0]
+        raw_signal = self.__reader.get_analogsignal_chunk(channel_ids=[channel_id])
+        float_signal = self.__reader.rescale_signal_raw_to_float(raw_signal, channel_ids=[channel_id])
+        return float_signal[start_index:end_index, 0]
 
     def readevents(self, channel_number, start_time=0, end_time=None):
-        pass
+        event_channel_index = [*self.__reader.header['event_channels']['id']].index(str(channel_number - 1))
+        event_timestamps = self.__reader.get_event_timestamps(event_channel_index=event_channel_index, t_start=start_time, t_stop=end_time)
+        return event_timestamps[0] * self.__reader._time_factor
